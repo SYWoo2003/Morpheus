1ae9b9f7a2bcf056f69314441ef015aff2782f5e
Woo-sang yun <wsy04018@naver.com>
2020-04-12 22:30:07 +0900

    Updates

---
+++ precache-manifest.8e11600c9717626c45cff256b6b67bd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f6b504197249a0aed2021cc1082294",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9af177358eb9c7535ce7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "66755d5e50866e16110e",
+    "url": "/static/js/2.8d45532c.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/static/js/2.8d45532c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af177358eb9c7535ce7",
+    "url": "/static/js/main.d36b53ed.chunk.js"
+  },
+  {
+    "revision": "3cb0648199cd1ff00b7d",
+    "url": "/static/js/runtime-main.e830bb82.js"
+  },
+  {
+    "revision": "2c238dd156d7cc5a348eb08da44a9113",
+    "url": "/static/media/logo.2c238dd1.png"
+  },
+  {
+    "revision": "e57613554d6f862ce02f878369f044a8",
+    "url": "/static/media/top.e5761355.gif"
+  }
+]);
